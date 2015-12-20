9b05e9694973ab99c69bc42ccec22fbf717de783
Dmitry <irvis@imega.ru>
2015-12-20 17:18:18 +0300

    Add docs

--- js/common.js
+++ js/common.js
@@ -26,27 +26,27 @@ shopcartApp.controller('cartCtrl', ['$filter', '$http', function ($filter, $http
     this.products = [
         {
             product_id: '678193d6-4740-4eb8-83b5-84f0a2174cde',
-            title: 'Apple iPhone 6S 16Gb',
+            title: 'iPhone 6S 16Gb',
             price: 749,
-            url: 'http://store.storeimages.cdn-apple.com/4861/as-images.apple.com/is/image/AppleInc/aos/published/images/i/ph/iphone6s/plus/iphone6s-plus-silver-select-2015?wid=270&fmt=png-alpha&qlt=95'
+            url: 'http://store.storeimages.cdn-apple.com/4861/as-images.apple.com/is/image/AppleInc/aos/published/images/i/ph/iphone6s/plus/iphone6s-plus-silver-select-2015?wid=130&fmt=png-alpha&qlt=95'
         },
         {
             product_id: 'ebc03f74-8468-4e3c-8481-228a7bc13be8',
-            title: 'Apple iPad mini 2 16Gb',
+            title: 'iPad mini 2 16Gb',
             price: 269,
-            url: 'http://store.storeimages.cdn-apple.com/4861/as-images.apple.com/is/image/AppleInc/aos/published/images/i/pa/ipad/mini/ipad-mini-retina-step1-white-2013_GEO_US?wid=270&fmt=png-alpha&qlt=95'
+            url: 'http://store.storeimages.cdn-apple.com/4861/as-images.apple.com/is/image/AppleInc/aos/published/images/i/pa/ipad/mini/ipad-mini-retina-step1-white-2013_GEO_US?wid=130&fmt=png-alpha&qlt=95'
         },
         {
             product_id: 'd969a708-7c75-4b32-b690-abe2e3f4f090',
-            title: 'Apple TV 32Gb',
+            title: 'TV 32Gb',
             price: 149,
-            url: 'http://store.storeimages.cdn-apple.com/4861/as-images.apple.com/is/image/AppleInc/aos/published/images/a/pp/apple/tv/apple-tv-hero-select-201510?wid=270&fmt=png-alpha&qlt=95'
+            url: 'http://store.storeimages.cdn-apple.com/4861/as-images.apple.com/is/image/AppleInc/aos/published/images/a/pp/apple/tv/apple-tv-hero-select-201510?wid=130&fmt=png-alpha&qlt=95'
         },
         {
             product_id: 'ad1d571a-3805-4590-9572-6c6fdf4a9faf',
             title: 'Mac mini 500 GB',
             price: 499,
-            url: 'http://store.storeimages.cdn-apple.com/4861/as-images.apple.com/is/image/AppleInc/aos/published/images/m/ac/mac/mini/mac-mini-select-hero-201505?wid=270&fmt=png-alpha&qlt=95'
+            url: 'http://store.storeimages.cdn-apple.com/4861/as-images.apple.com/is/image/AppleInc/aos/published/images/m/ac/mac/mini/mac-mini-select-hero-201505?wid=130&fmt=png-alpha&qlt=95'
         }
     ];
     this.basket = [];
@@ -61,14 +61,6 @@ shopcartApp.controller('cartCtrl', ['$filter', '$http', function ($filter, $http
                 url: product.url,
                 quantity: 1
             });
-        } else {
-            this.basket[index] = {
-                product_id: product.product_id,
-                title: product.title,
-                price: product.price,
-                url: product.url,
-                quantity: this.basket[index].quantity + 1
-            }
         }
         $http.put('http://demo-teleport.imega.ru/cart', {
             price: product.price,
